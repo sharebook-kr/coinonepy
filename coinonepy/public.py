deb215c94399fb82ae3b972c2d7ad511faa3f381
Brayden Jo <brayden.jo@outlook.com>
2021-11-21 15:19:10 +0900

    add get_tickers

--- coinonepy/public.py
+++ coinonepy/public.py
@@ -1,16 +1,22 @@
 import requests 
 
 
-def get_current_price(ticker="btc", verbose=False):
+def get_tickers():
+    url = "https://api.coinone.co.kr/ticker/"
+    params = {"currency": "all"}
+    resp = requests.get(url, params=params)
+    data = resp.json()
+    reserved_keys = ['result', 'errorCode', 'timestamp']
+    tickers = [k for k in data.keys() if k not in reserved_keys]
+    return tickers
+
+
+def get_current_price(ticker="btc"):
     url = "https://api.coinone.co.kr/ticker/"
     params = {"currency": ticker}
     resp = requests.get(url, params=params)
     data = resp.json()
-    cur_price = float(data['last'])
-    if verbose:
-        return data
-    else:
-        return cur_price
+    return data
 
 
 def get_orderbook(ticker):
@@ -23,11 +29,13 @@ def get_orderbook(ticker):
 
 if __name__ == "__main__":
     import pprint 
+    tickers = get_tickers()
+    print(tickers)
 
     btc_price = get_current_price("xrp")
     print(btc_price)
 
-    btc_price = get_current_price("xrp", verbose=True)
+    btc_price = get_current_price("all")
     print(btc_price)
 
     order = get_orderbook("btc")
